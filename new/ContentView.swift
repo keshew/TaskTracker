5c0e53014f359675c8db2c18ef1b791fddda00cf
keshew <artyom.korotckov2017@yandex.ru>
2025-07-09 14:49:04 +0500

    Initial Commit

---
+++ new/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  new
+//
+//  Created by Артём Коротков on 09.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ new/newApp.swift
@@ -0,0 +1,17 @@
+//
+//  newApp.swift
+//  new
+//
+//  Created by Артём Коротков on 09.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
